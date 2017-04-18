b0c032c2fd6a09bbe36060fd542327b4a832bce0
Dongwook Shin <nawook0331@nate.com>
2017-04-18 18:12:07 +0900

    connect5

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var path    = require('path');
 var MongoClient = require('mongodb').MongoClient;
 
 var uri = "mongodb://admin:admin@cluster0-shard-00-00-qkzvq.mongodb.net:27017,cluster0-shard-00-01-qkzvq.mongodb.net:27017,cluster0-shard-00-02-qkzvq.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-MongoClient.connect(uri, function(err, db) {
-  db.close();
+MongoClient.connect(uri, function(err) {
+  console.log("DB ERROR : ", err);
 });
 
 app.use(express.static(path.join(__dirname,"public")));
