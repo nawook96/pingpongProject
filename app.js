5b975280469e641b4d3751f916034400a7d2c5a3
Dongwook Shin <nawook0331@nate.com>
2017-04-18 17:47:21 +0900

    DB connect2

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
 var path    = require('path');
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-qkzvq.mongodb.net:27017,cluster0-shard-00-01-qkzvq.mongodb.net:27017,cluster0-shard-00-02-qkzvq.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+var db = mongoose.connection;
+db.once("open", function()
+{
+  console.log("DB connected!");
+});
+db.on("error", function(err){
+  console.log("DB ERROR : ", err);
+});
+
 
 app.use(express.static(path.join(__dirname,"public")));
 
