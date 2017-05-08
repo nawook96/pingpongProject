cb62b40f95a2df406146bc5b50013b258287ed3f
Dongwook Shin <nawook0331@nate.com>
2017-05-09 01:39:07 +0900

    commit

--- app.js
+++ app.js
@@ -19,11 +19,22 @@ http.listen(port, function(){
 });
 
 var objects = {};
+var posi = 0;
 
 // 접속 유저의 정보를 저장하는 개체 생성
 io.on('connection', function(socket){
   console.log('user connected: ', socket.id);
-  objects[socket.id] = new UserObject();
+  switch(posi){
+    case 0:
+      objects[socket.id] = new UserObject(socket.id, "LEFT");
+      posi = 1;
+      break;
+    case 1:
+      objects[socket.id] = new UserObject(socket.id, "RIGHT");
+      posi = 0;
+      break;
+  }
+
   io.to(socket.id).emit('connected', GAME_SETTINGS);
 
 // 접속 종료시 해당 개체 삭제, discinnect 이벤트를 client에 보냄
@@ -45,7 +56,7 @@ io.on('connection', function(socket){
 
 var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
 var GAME_SETTINGS = {
-  WIDTH : 600, HEIGHT : 400, BACKGROUND_COLOR : "#FFFFFF"
+  WIDTH : 700, HEIGHT : 400, BORDER_WIDTH : 15, BACKGROUND_COLOR : "#FFFFFF"
 };
 
 // 10ms 마다 키가 눌려져 있는지 안 눌려져 있는지 확인 후 눌려져 있다면 좌표 이동 및 정보 업데이트
@@ -53,7 +64,9 @@ var update = setInterval(function(){
   var idArray=[];
   var statusArray={};
   for(var id in io.sockets.clients().connected){
+    if(objects[id].keypress[LEFT])  objects[id].status.x -= 0;
     if(objects[id].keypress[UP])    objects[id].status.y -= 2;
+    if(objects[id].keypress[RIGHT]) objects[id].status.x += 0;
     if(objects[id].keypress[DOWN])  objects[id].status.y += 2;
 
     idArray.push(id);
@@ -63,15 +76,23 @@ var update = setInterval(function(){
 },10);
 
 // 유저 개체의 생성자, x,y 좌표, width, height, color 저장
-function UserObject() {
+function UserObject(id, position) {
   var color="#";
   for(var i = 0; i < 6; i++ ){
     color += (Math.floor(Math.random()*16)).toString(16);
   }
   this.status = {};
+  this.status.height = 80;
+  this.status.width = 15;
+  switch(position){
+    case "LEFT":
+      this.status.x = 0;
+      break;
+    case "RIGHT":
+      this.status.x = GAME_SETTINGS.WIDTH -this.status.width;
+      break;
+  }
   this.status.y = 0;
-  this.status.height = 100;
-  this.status.width = 20;
   this.status.color = color;
   this.keypress = [];
 }
